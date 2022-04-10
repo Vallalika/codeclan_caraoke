c7445df3e5f0c6c32cf9cd8e0f564dee67fdf13b
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-10 13:16:39 +0100

    tab_test and tab class fixed

--- tests/tab_tests.py
+++ tests/tab_tests.py
@@ -8,11 +8,12 @@ from classes.tab import Tab
 class TestTab(unittest.TestCase):
 
     def setUp(self):
+        self.lazy_song = Song("The Lazy Song", "Bruno Mars", "Pop")
         self.jane = Guest("Jane", 50.00, self.lazy_song)
         self.janes_tab = Tab(self.jane)
     
-    @unittest.skip
+    # @unittest.skip
     def test_set_up(self):
-        self.assertEqual(self.jane.name,self.janes_tab.name)
+        self.assertEqual(self.jane.name,self.janes_tab.guest_name)
         self.assertEqual({},self.janes_tab.bought_items)
         self.assertEqual(0.00,self.janes_tab.total_spent)
